52a3f58e48c0ac5bc3074a6d7d59f96de08c9f14
obasys <obasystiuk@gmail.com>
2020-03-22 20:08:09 +0200

    Included new sections into index.js

--- src/display/pages/Home/index.js
+++ src/display/pages/Home/index.js
@@ -6,12 +6,18 @@ import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 
 import Benefits from "./Benefits";
+import Clients from "./Clients";
+import Technologies from "./Technologies";
+import Team from "./Team";
 
 const Home = () => {
   	return (
 		<div>
 			<Container>
 				<Benefits />
+                <Clients />
+                <Technologies />
+                <Team />
 			</Container>
 		</div>
   	);
