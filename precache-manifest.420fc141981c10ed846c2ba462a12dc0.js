97c962e41eafede942867765fa40f5e1ba14be31
obasys <obasystiuk@gmail.com>
2020-05-27 10:05:31 +0300

    Updates

---
+++ precache-manifest.420fc141981c10ed846c2ba462a12dc0.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b53f9dffc73807d33dcc6c4fad1302f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aaafd4a9a7f2f40a3a5c",
+    "url": "/static/js/2.19cb5795.chunk.js"
+  },
+  {
+    "revision": "dd65da266c298bd86b874e191469c08c",
+    "url": "/static/js/2.19cb5795.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0e21139ca783e6a519cf",
+    "url": "/static/js/main.ff0d1556.chunk.js"
+  },
+  {
+    "revision": "f2effc4e13c23bb642d6",
+    "url": "/static/js/runtime-main.f4bc4243.js"
+  },
+  {
+    "revision": "0329712e589758ec8ac1d28175b56aab",
+    "url": "/static/media/1.0329712e.svg"
+  },
+  {
+    "revision": "57dda7cf99f910c734beaf958eea76b6",
+    "url": "/static/media/2.57dda7cf.svg"
+  },
+  {
+    "revision": "3abb8505d6ce394ec29497c688913296",
+    "url": "/static/media/3.3abb8505.svg"
+  },
+  {
+    "revision": "b67debb7a78b9aab8ffb98d4ad00c273",
+    "url": "/static/media/4.b67debb7.svg"
+  },
+  {
+    "revision": "660234eb134cfc0863fb4b1a140d4585",
+    "url": "/static/media/5.660234eb.svg"
+  },
+  {
+    "revision": "797d817edb656d119d2e37da1dc3a130",
+    "url": "/static/media/6.797d817e.svg"
+  },
+  {
+    "revision": "e78d6b0a977945dca969d3b912caa73b",
+    "url": "/static/media/alphacon.e78d6b0a.png"
+  },
+  {
+    "revision": "b529723b58b911f043bd1a241dfa7dde",
+    "url": "/static/media/aok-artboard.b529723b.png"
+  },
+  {
+    "revision": "713ec2b770c42e06efdc75fef463ee87",
+    "url": "/static/media/aok.713ec2b7.png"
+  },
+  {
+    "revision": "7a8a06141da0be338ecb0228ddcbd6d7",
+    "url": "/static/media/arrow.7a8a0614.svg"
+  },
+  {
+    "revision": "d7876d48e80112e766d737346a616707",
+    "url": "/static/media/bitcoin.d7876d48.svg"
+  },
+  {
+    "revision": "8d78799de1077932a56907e44d729869",
+    "url": "/static/media/flask.8d78799d.svg"
+  },
+  {
+    "revision": "218944185d0d95254ba340fd0c81700a",
+    "url": "/static/media/harry-artboard.21894418.jpg"
+  },
+  {
+    "revision": "a53ab7f34d3e39bba451aeb3a233a270",
+    "url": "/static/media/header.a53ab7f3.svg"
+  },
+  {
+    "revision": "491dd51e96e167204500c75e91d26462",
+    "url": "/static/media/karbonara-artboard.491dd51e.jpg"
+  },
+  {
+    "revision": "d52287738b55788ea70175d566102073",
+    "url": "/static/media/laravel.d5228773.svg"
+  },
+  {
+    "revision": "3ea969e8233d4e123652fc7bea01be1d",
+    "url": "/static/media/logo.3ea969e8.svg"
+  },
+  {
+    "revision": "a73455fba6f1a56e007e713467817540",
+    "url": "/static/media/microbitcoin-artboard.a73455fb.jpg"
+  },
+  {
+    "revision": "63ce4c42a1de39cf2a6af507f78edaf3",
+    "url": "/static/media/olexb.63ce4c42.svg"
+  },
+  {
+    "revision": "4e36fbdc5fe9703e6c3dc4d6ea41454a",
+    "url": "/static/media/olexh.4e36fbdc.svg"
+  },
+  {
+    "revision": "05a3e30fdf60ebe9d00cf4725d44bd42",
+    "url": "/static/media/react.05a3e30f.svg"
+  },
+  {
+    "revision": "85772647236e76b8e772c14c092a28ac",
+    "url": "/static/media/redux.85772647.svg"
+  },
+  {
+    "revision": "e2e40c1a3c1422e708a5588b383ec88f",
+    "url": "/static/media/sugarchain.e2e40c1a.svg"
+  },
+  {
+    "revision": "6f83f74d3d9e3e2bc737f66c599156fa",
+    "url": "/static/media/tensorflow.6f83f74d.svg"
+  },
+  {
+    "revision": "17e16677bbc1dfe353abc6943bdb52c1",
+    "url": "/static/media/volbil.17e16677.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5395c13c2e8c9e25c8fe9b163ec16ecd.js"
+  "/precache-manifest.420fc141981c10ed846c2ba462a12dc0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
